554870116f0748ff569e511ff77927c9d813440e
Srinivas nalla <nallasrinivas678@outlook.com>
2020-09-16 18:54:02 -0400

    Add project files.

---
+++ Ch02/02_01/End/CSharpQ/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpQ
+{
+    /// 1.	What’s REFERENCE TYPES? 
+    ///     
+    ///     Reference types are objects that store references 
+    ///     to the actual data. 
+    ///     
+    ///     Class, Interface, delegate, object & string --> Reference types
+
+
+    /// Class
+    class Program
+    {
+        // Object
+        static object Book { get; set; } = "Srinivas Nalla Book";
+
+        // Delegate - function pointers
+        //when you invoke delegat it return function with computed value
+        //Link for more details : https://www.pluralsight.com/guides/how-why-to-use-delegates-csharp
+        delegate void del(string value);
+
+        static void Main(string[] args)
+        {
+            del d = x => Console.WriteLine(x);
+            d.Invoke(Book.ToString());
+            Console.ReadLine();
+        }
+    }
+}
+
+    /// Interface - Reference Type
+    interface Student
+    {
+        // Some content
+    }
+
+    /// ==================================================================================
+    /// 2.	What are VALUE TYPES? 
+    /// 
+    ///     Value types actually hold values. Assigning one value type 
+    ///     to another literally copies that value.
+
+    /// Structs, enums, bools, numberic types 
+    public struct Book
+    {
+        public string title;
+        bool isRecommended;
+
+        // Floating point, integral types (sbyte, byte, char etc) https://goo.gl/k7wqDu
+        public decimal price;
+    }
+
+    enum Days { Sat, Sun, Mon, Tue, Wed, Thu, Fri };
+
---
+++ Ch02/02_03/End/AbstractDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 1.	What’s an ABSTRACT CLASS – 
+///     
+///     Similar to interfaces but cannot be instantiated, and are frequently 
+///     either partially implemented, or not at all implemented. Different from interface in that:
+///         a. Abstract classes DO contain code & data
+///         b. A class can inherit from only one abstract class but can implement an unlimited number of 
+///             interfaces
+///         c. You can specify methods as ‘virtual’ to force derived classes to create its own implementation.
+/// </summary>
+
+namespace AbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.Name = "Abstract - Srinivas Nalla";
+            S.PrintName();
+            Console.ReadLine();
+        }
+    }
+ 
+    abstract class Person
+    {
+        public int Age { get; set; } = 29;
+        public abstract void PrintName();
+    }
+
+    class Student : Person
+    {        
+        public string Name { get; set; }
+        public override void PrintName()
+        {
+            Console.WriteLine($"Student name is {Name} and is {Age}.");
+        }
+    }
+}
