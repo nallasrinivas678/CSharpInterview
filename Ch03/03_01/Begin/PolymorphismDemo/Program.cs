c07307549402f4f679c97d62f13b42af80ca240a
Srinivas nalla <nallasrinivas678@outlook.com>
2020-09-16 19:30:32 -0400

    Polimorphism with employee example

--- Ch03/03_01/Begin/PolymorphismDemo/Program.cs
+++ Ch03/03_01/Begin/PolymorphismDemo/Program.cs
@@ -11,12 +11,42 @@ using System.Threading.Tasks;
 /// </summary>
 namespace PolymorphismDemo
 {
-
+    public class Employee
+    {
+        public string EmployeeName { get; set; } = "Srinivas Nalla";
+        public int Age { get; set; } = 29;
+        public virtual void CalculateWeeklySalary(int weeklyHrs, int wage)
+        {
+            var salary = 40 * wage;
+            Console.WriteLine($" Name {this.EmployeeName } - \n This Employee worked {weeklyHrs} hrs. " +
+                $"Paid for 40 hrs w/o overtime at {wage}/hr => {salary}");
+            
+        }
+    }
+    public class Contractor : Employee
+    {
+        public override void CalculateWeeklySalary(int weeklyHrs, int wage)
+        {
+            var salary = weeklyHrs * wage;
+            Console.WriteLine($"Name {this.EmployeeName } - \n This contrat employee worked {weeklyHrs} hrs. " +
+                $"Paid for {weeklyHrs} hrs with overtime at {wage}/hr => {salary}");
+            
+        }
+    }
     public class Program
     {
         private static void Main(string[] args)
         {
             //TODO
+            const int hours = 50, wage = 70;
+            Employee person = new Employee();
+            person.CalculateWeeklySalary(hours,wage);
+
+            var contractor = new Contractor();
+            contractor.CalculateWeeklySalary(hours, wage);
+
+            Console.ReadLine();
         }
+
     }
 }
